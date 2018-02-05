2fd096db21b82c62d4712ca15733c1ee717d2012
Torevill <Torevill@gmail.com>
2018-02-05 18:04:43 +0300

    Junit commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,11 @@
 package main;
+
 import sources.SimpleGUI;
 
 public class Main {
     public static void main(String[] args) {
         SimpleGUI gui = new SimpleGUI();
         gui.setVisible(true);
+        System.out.println("End");
     }
 }
